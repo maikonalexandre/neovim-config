b5870e0a4cfd8fa0215080703c10372d40f092ac
Maike <maikonalexandre574@gmail.com>
2024-09-07 09:00:43 -0300

    fix some issues

--- init.lua
+++ init.lua
@@ -15,14 +15,31 @@ vim.keymap.set("i", "<C-s>", "<Esc>:w!<cr>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-d>", "<Esc>:wq!<cr>", { noremap = true, silent = true })
 
 -- normal mode
-vim.keymap.set("i", "<C-s>", ":w!<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-s>", ":w!<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-d>", "<Esc>:wq!<cr>", { noremap = true, silent = true })
 
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+vim.keymap.set("n", "<leader>su", "<C-w>k", { desc = "Go to upper window " })
+vim.keymap.set("n", "<leader>sl", "<C-w>l", { desc = "Go to left window " })
+vim.keymap.set("n", "<leader>sr", "<C-w>h", { desc = "Go to right window " })
+vim.keymap.set("n", "<leader>sd", "<C-w>j", { desc = "Go to down window " })
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
 -- disable arrow in normal mode
-vim.keymap.set('n', '<Up>', '<NOP>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Down>', '<NOP>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Left>', '<NOP>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Right>', '<NOP>', { noremap = true, silent = true })
+vim.keymap.set("n", "<Up>", "<NOP>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", "<NOP>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", "<NOP>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", "<NOP>", { noremap = true, silent = true })
 
 -- open and close terminal right aside
 -- vim.keymap.set("n", "<C-t>", ":rightbelow vsplit term://fish<CR>", { noremap = true, silent = true })
@@ -36,15 +53,19 @@ vim.keymap.set('n', '<Right>', '<NOP>', { noremap = true, silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+   change_detection = {
+    notify = false,
+  },
+})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,46 +1,46 @@
 return {
 	{
-	  "williamboman/mason.nvim",
-	  lazy = false,
-	  config = function()
-		require("mason").setup()
-	  end,
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
 	},
 	{
-	  "williamboman/mason-lspconfig.nvim",
-	  lazy = false,
-	  opts = {
-		auto_install = true,
-	  },
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
-	  "neovim/nvim-lspconfig",
-	  lazy = false,
-	  config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  
-		local lspconfig = require("lspconfig")
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		lspconfig.tsserver.setup({
-		  capabilites = capabilities,
-		})
+			local lspconfig = require("lspconfig")
 
-		lspconfig.html.setup({
-		  capabilites = capabilities,
-		})
+			lspconfig.tsserver.setup({
+				capabilites = capabilities,
+			})
 
-		lspconfig.lua_ls.setup({
-		  capabilites = capabilities,
-		})
-		
-		lspconfig.biome.setup({
-			capabilities = capabilities,
-		})
+			lspconfig.html.setup({
+				capabilites = capabilities,
+			})
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-	  end,
+			lspconfig.lua_ls.setup({
+				capabilites = capabilities,
+			})
+
+			lspconfig.biome.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
 	},
-  }
+}
