7219c1ddf0db63ca08dcdb19c3911d58e130fabf
Maike <maikonalexandre574@gmail.com>
2024-09-07 07:50:50 -0300

    remove neotree add nvim tree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
-        defaults = { file_ignore_patterns = {"node_modules"} },
+        defaults = { file_ignore_patterns = { "node_modules" } },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
@@ -17,8 +17,11 @@ return {
       })
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      --vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      --vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      --vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 
       require("telescope").load_extension("ui-select")
     end,
