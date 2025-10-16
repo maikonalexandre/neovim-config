ec58e522521756fdecd983a8c3fab6b91de36574
Maike <maikonalexandre574@gmail.com>
2025-10-16 16:05:58 -0300

    add format comand to lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,52 +1,55 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.ts_ls.setup({
-				capabilites = capabilities,
-			})
+      lspconfig.ts_ls.setup({
+        capabilites = capabilities,
+      })
 
-			lspconfig.html.setup({
-				capabilites = capabilities,
-			})
+      lspconfig.html.setup({
+        capabilites = capabilities,
+      })
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.biome.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.biome.setup({
+        capabilities = capabilities,
+      })
 
       lspconfig.gopls.setup({
         capabilities = capabilities,
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {desc = "Hover text"})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {desc = "See definition"})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {desc = "See references"})
-			vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, {desc = "See code actions"})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover text" })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "See definition" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "See references" })
+      vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, { desc = "See code actions" })
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Renomear símbolo" })
 
-		end,
-	},
+      vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, { desc = "Formatar arquivo com LSP" })
+    end,
+  },
 }
